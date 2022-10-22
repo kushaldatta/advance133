94ea181004badbd30948dc31446a5ee056ec3a29
kushaldatta <96032544+kushaldatta@users.noreply.github.com>
2022-10-22 09:33:20 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,47 @@
+var status = "";
+var objects = [];
+
+function preload() {
+    img = loadImage("Fruit.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    object_detection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 600, 500);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill('#FF0000');
+            stroke('#FF0000');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y);
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("number").innerHTML = "There are 11 big fruits from which CoCoSSD model detected 1 fruit."
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("CoCoSSD Model Is Loaded.")
+    status = true;
+    object_detection.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function back() {
+    window.location = "index.html";
+}
